49a6f8f85aae268c128c1a87f0eccdf80a8d6b21
Alceatraz <alceatraz@blacktech.studio>
2021-02-03 16:41:07 +0800

    0.4.8

--- src/main/java/studio/blacktech/furryblackplus/Driver.java
+++ src/main/java/studio/blacktech/furryblackplus/Driver.java
@@ -62,7 +62,7 @@ public final class Driver {
     // ==========================================================================================================================================================
 
 
-    private final static String APP_VERSION = "0.4.7";
+    private final static String APP_VERSION = "0.4.8";
 
 
     private final static long BOOT_TIME = System.currentTimeMillis();
