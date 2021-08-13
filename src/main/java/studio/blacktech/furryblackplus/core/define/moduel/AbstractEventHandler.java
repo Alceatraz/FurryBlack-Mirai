ea8c44d860120bd8518a94e28632974435618b01
Alceatraz <alceatraz@blacktech.studio>
2021-08-13 15:37:11 +0800

    Update README and LICENSE and all CopyRight

--- src/main/java/studio/blacktech/furryblackplus/core/define/moduel/AbstractEventHandler.java
+++ src/main/java/studio/blacktech/furryblackplus/core/define/moduel/AbstractEventHandler.java
@@ -2,14 +2,14 @@
  * Copyright (C) 2021 Alceatraz @ BlackTechStudio
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the BTS Anti Commercial & GNU Affero General.
+ * it under the terms of the BTS Anti-Commercial & GNU Affero General.
 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * BTS Anti Commercial & GNU Affero General Public License for more details.
+ * BTS Anti-Commercial & GNU Affero General Public License for more details.
  *
- * You should have received a copy of the BTS Anti Commercial & GNU Affero
+ * You should have received a copy of the BTS Anti-Commercial & GNU Affero
  * General Public License along with this program in README or LICENSE.
  */
 package studio.blacktech.furryblackplus.core.define.moduel;
