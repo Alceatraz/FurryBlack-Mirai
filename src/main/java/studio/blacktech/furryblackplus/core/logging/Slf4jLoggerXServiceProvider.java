294a129225670851a8081799401892cc44616a4f
Alceatraz <alceatraz@blacktech.studio>
2023-08-26 18:45:08 +0800

    3.0.3

--- src/main/java/studio/blacktech/furryblackplus/core/logging/Slf4jLoggerXServiceProvider.java
+++ src/main/java/studio/blacktech/furryblackplus/core/logging/Slf4jLoggerXServiceProvider.java
@@ -2,12 +2,16 @@ package studio.blacktech.furryblackplus.core.logging;
 
 import org.slf4j.ILoggerFactory;
 import org.slf4j.IMarkerFactory;
+import org.slf4j.helpers.BasicMDCAdapter;
+import org.slf4j.helpers.BasicMarkerFactory;
 import org.slf4j.spi.MDCAdapter;
 import org.slf4j.spi.SLF4JServiceProvider;
 
 public class Slf4jLoggerXServiceProvider implements SLF4JServiceProvider {
 
+  private final BasicMDCAdapter basicMDCAdapter = new BasicMDCAdapter();
   private final Slf4jLoggerXFactory loggerFactory = new Slf4jLoggerXFactory();
+  private final BasicMarkerFactory basicMarkerFactory = new BasicMarkerFactory();
 
   @Override
   public void initialize() {
@@ -26,12 +30,12 @@ public class Slf4jLoggerXServiceProvider implements SLF4JServiceProvider {
 
   @Override
   public IMarkerFactory getMarkerFactory() {
-    return null;
+    return basicMarkerFactory;
   }
 
   @Override
   public MDCAdapter getMDCAdapter() {
-    return null;
+    return basicMDCAdapter;
   }
 
 }
